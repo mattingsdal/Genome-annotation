7cd98e0e1ef444e8f00388a25bf8ce7b5a398c18
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-05-20 09:21:04 +0200

    Update 02.GenemarkES.sh

--- 02.GenemarkES.sh
+++ 02.GenemarkES.sh
@@ -1,7 +1,13 @@
-# run GeneMark
+# run GeneMark-ES
+# hmm file to MAKER is gmhmm.mod
 
-wd=
-genome=
-module load genemark-es
+wd=/usit/abel/u1/mortema/genome
+genome=mini.fasta
 
-gmes_petap.pl --min_contig 20000 --cores 4 --ES --sequence $genome
+cd $wd
+mkdir $genome.genemark
+cd $genome.genemark
+
+~/software/maker/exe/gm_et_linux_64/gmes_petap/gmes_petap.pl --min_contig 20000 --cores 4 --ES --sequence ../$genome
+
+module purge
