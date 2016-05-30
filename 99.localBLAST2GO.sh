897455a677d3b25250a677a43085e20ab1f9f9a2
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2016-05-30 14:31:15 +0200

    Update 99.localBLAST2GO.sh

--- 99.localBLAST2GO.sh
+++ 99.localBLAST2GO.sh
@@ -1,2 +1,7 @@
 
+## https://www.blast2go.com/support/blog/22-blast2goblog/110-local-blast2go-database-installation
+mysql -h localhost -u root -p  < b2gdb.sql
+mysql -h localhost -u root -p -e "GRANT ALL ON b2gdb.* TO 'blast2go'@'localhost' IDENTIFIED BY 'blast4it';"
+mysql -h localhost -u root -p -e "FLUSH PRIVILEGES;"
 
+mysql -s -h localhost -u root -p b2gdb < go_201605-assocdb-data
