3a133dd7fd305beb996916f3003863db33f80e5b
Morten Mattingsdal <morten.mattingsdal@gmail.com>
2017-03-10 12:25:10 +0100

    Update MAKER.MPI.sh

--- MAKER.MPI.sh
+++ MAKER.MPI.sh
@@ -1,5 +1,5 @@
 module load maker
 module load openmpi.intel
 export I_MPI_PMI_LIBRARY=/opt/slurm/lib64/libpmi.so
-srun -A cees -n 100 --mem-per-cpu=2G --time=168:00:00 maker > maker.mpi.out 2> maker.mpi.err &
+srun -A cees -n 150 --mem-per-cpu=2G --time=4320 maker > maker.mpi.out 2> maker.mpi.err &
 
